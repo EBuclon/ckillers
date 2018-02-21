a8eec94f1ecf67cb61cfc1584a2e322e1999b34c
Eloi <32641086+S-Penguin@users.noreply.github.com>
2018-02-21 14:08:34 +0100

    Add files via upload

---
+++ src/main/java/fr/ck/servlet/requetes/SupprimerCreneauServlet.java
@@ -0,0 +1,31 @@
+package fr.ck.servlet.requetes;
+
+import fr.ck.entite.Creneau;
+import fr.ck.servlet.GenericServlet;
+import fr.ck.Service.Service;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/supprimerCreneau")
+public class SupprimerCreneauServlet extends GenericServlet{
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Integer idCreneau = Integer.parseInt(req.getParameter("idCreneau"));
+        Service.getInstance().supprimerCreneau(idCreneau);
+        resp.sendRedirect("/creneauPartie");
+
+        TemplateEngine templateEngine = this.createTemplateEngine(req.getServletContext());
+        WebContext context = new WebContext(req, resp, req.getServletContext());
+        context.setVariable("creneau",Service.getInstance().getCreneau(idCreneau));
+
+        //templateEngine.process("supprimerCreneau", context, resp.getWriter());
+
+    }
+
+}
