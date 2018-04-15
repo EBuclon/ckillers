0229fc4e23f5e78dc5461e3912616b1ea3381ace
Eloi <32641086+S-Penguin@users.noreply.github.com>
2018-04-15 23:13:57 +0200

    Add files via upload

---
+++ src/main/java/fr/ck/servlet/templates/QuiServlet.java
@@ -0,0 +1,34 @@
+package fr.ck.servlet.templates;
+
+import fr.ck.Service.Service;
+import fr.ck.entite.Inscrit;
+import fr.ck.entite.Partenaire;
+import fr.ck.servlet.GenericServlet;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/quisommesnous")
+public class QuiServlet extends GenericServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        WebContext context = new WebContext(req, resp, req.getServletContext());
+
+        String identifiantUtilisateur = (String) req.getSession().getAttribute("utilisateur");
+        String statutUtil = (String) req.getSession().getAttribute("statut");
+        if(identifiantUtilisateur == null || "".equals(identifiantUtilisateur)) {
+            context.setVariable("inscrit", new Inscrit("Visiteur"));
+        }else{
+            context.setVariable("inscrit", new Inscrit(identifiantUtilisateur,statutUtil));
+        }
+        TemplateEngine templateEngine = createTemplateEngine(req.getServletContext());
+        templateEngine.process("quiSommesNous", context, resp.getWriter());
+
+    }
+}
